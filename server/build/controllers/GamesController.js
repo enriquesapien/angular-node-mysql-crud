1ba32777ecbe7af6b2412aac1d313773ea372116
enriquesapien <enrique.sapien@gmail.com>
2019-08-18 16:29:15 -0500

    Remove build folder

--- server/build/controllers/GamesController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class GamesController {
-    index(req, res) {
-        res.send('Hello from Games controller');
-    }
-}
-const gamesController = new GamesController();
-exports.default = gamesController;
