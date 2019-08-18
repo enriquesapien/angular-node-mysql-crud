1f487b0722ac63843f68c04687bfffb190cbb550
enriquesapien <enrique.sapien@gmail.com>
2019-08-18 12:39:28 -0500

    Add dev script for nodemon

--- server/build/index.js
+++ server/build/index.js
@@ -2,3 +2,4 @@
 console.log('hola desde server');
 console.log('otro log');
 console.log('otro');
+console.log('otro mas para nodemon');
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,3 +3,5 @@ console.log('hola desde server');
 console.log('otro log');
 
 console.log('otro');
+
+console.log('otro mas para nodemon');
